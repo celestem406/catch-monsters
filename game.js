26e9c3242274c4ad7d769c1f50ec2b8c4a44d393
celestem406 <celeste.mancilla@cristoreysj.org>
2021-03-11 14:26:02 -0800

    changed up code

--- game.js
+++ game.js
@@ -26,14 +26,6 @@ heroImage.onload = function () {
 };
 heroImage.src = "images/hero.png";
 // Load the monster image
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
-  // show the monster image
-  monsterReady = true;
-};
-monsterImage.src = "images/monster.png";
-// Load the monster image
 var monster1Ready = false;
 var monster1Image = new Image();
 monster1Image.onload = function () {
@@ -77,7 +69,7 @@ var reset = function () {
   monster1.y = 32 + (Math.random() * (canvas.height - 64));
   
   monster2.x = 32 + (Math.random() * (canvas.width - 64));
-  monster12y = 32 + (Math.random() * (canvas.height - 64));
+  monster2.y = 32 + (Math.random() * (canvas.height - 64));
 
 
 };
@@ -121,90 +113,14 @@ if (39 in keysDown) { // Player is holding right key
     ++monstersCaught;
     reset();
   }
-};
-// Draw everything on the canvas
-var render = function () {
-  if (bgReady) {
-    ctx.drawImage(bgImage, 0, 0);
-  }
-  if (heroReady) {
-    ctx.drawImage(heroImage, hero.x, hero.y);
-  }
-  if (monsterReady) {
-    ctx.drawImage(monsterImage, monster.x, monster.y);/*
-*/
-// Load the monster image
-var monster2Ready = false;
-var monster2Image = new Image();
-monster2Image.onload = function () {
-  // show the monster image
-  monster2Ready = true;
-};
-monster2Image.src = "images/monster.png";
-// Create the game objects
-var hero = {
-  speed: 256, // movement speed of hero in pixels per second
-  height: 36,
-  width: 31
-};
-var monster = {};
-var monstersCaught = 0;
-// Handle keyboard controls
-var keysDown = {};
-// Check for keys pressed where key represents the keycode captured
-addEventListener("keydown", function (key) {
-  keysDown[key.keyCode] = true;
-}, false);
-addEventListener("keyup", function (key) {
-  delete keysDown[key.keyCode];
-}, false);
-// Reset the player and monster positions when player catches a monster
-var reset = function () {
-  // Reset player's position to centre of canvas
-  hero.x = canvas.width /2;
-  hero.y = canvas.height /2 ;
-  // Place the monster somewhere on the canvas randomly
-  monster.x = 32 + (Math.random() * (canvas.width - 64));
-  monster.y = 32 + (Math.random() * (canvas.height - 64));
-};
-// Update game objects - change player position based on key pressed
-var update = function (modifier) {
-  if (38 in keysDown) { // Player is holding up key
-   new_position =  hero.y - (hero.speed * modifier);
-    if (new_position > 0) {
-        hero.y = new_position;
-    }
-}
-if (40 in keysDown) { // Player is holding down key
-    new_position =  hero.y + (hero.speed * modifier);
-    console.log(new_position)
-    console.log(hero.height)
-    if (new_position < (canvas.height - hero.height)) {
-        hero.y = new_position;
-    }
-}
-if (37 in keysDown) { // Player is holding left key
-    new_position =  hero.x - (hero.speed * modifier);
-    if (new_position > 0) {
-        hero.x = new_position;
-    }
-}
-if (39 in keysDown) { // Player is holding right key
-    new_position =  hero.x + (hero.speed * modifier);
-    console.log(new_position)
-    console.log(hero.width)
-    if (new_position < (canvas.width - hero.width)) {
-        hero.x = new_position;
-    }
-}
-  // Check if player and monster collider
   if (
-    hero.x <= (monster.x + 32)
-    && monster.x <= (hero.x + 32)
-    && hero.y <= (monster.y + 32)
-    && monster.y <= (hero.y + 32)
-  ) {
-    ++monstersCaught;
+hero.x <= (monster2.x + 32)
+    && monster2.x <= (hero.x + 32)
+    && hero.y <= (monster2.y + 32)
+    && monster2.y <= (hero.y + 32)
+    ){
+
+     ++monstersCaught;
     reset();
   }
 };
@@ -216,14 +132,18 @@ var render = function () {
   if (heroReady) {
     ctx.drawImage(heroImage, hero.x, hero.y);
   }
-  if (monsterReady) {
-    ctx.drawImage(monsterImage, monster.x, monster.y); 
+  if (monster1Ready) {
+    ctx.drawImage(monster1Image, monster1.x, monster1.y);
   }
+  if (monster2Ready) {
+    ctx.drawImage(monster2Image, monster2.x, monster2.y);
+  }
+
   // Display score and time
   ctx.fillStyle = "rgb(250, 250, 250)";
   ctx.font = "24px Helvetica";
   ctx.textAlign = "left";
-  ctx.textBaseline = "top";
+  ctx.textBaseline = "top"; 
   ctx.fillText("Monsters caught: " + monstersCaught, 20, 20);
   ctx.fillText("Time: " + count, 20, 50);
   // Display game over message when timer finished
@@ -293,7 +213,8 @@ var counter =function(){
        finished = true;
        count=0;
        // hider monster and hero
-       monsterReady=false;
+       monster1Ready=false;
+       monster2Ready=false;
        heroReady=false;
     }
 }
